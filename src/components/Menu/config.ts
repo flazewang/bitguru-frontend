4dda026476bc37b9dd7379d46a2c3c5ce8c3a541
flazewang <71084388+flazewang@users.noreply.github.com>
2021-04-16 21:29:22 +0800

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -31,61 +31,12 @@ const config: MenuEntry[] = [
     href: '/pools',
   },
   {
-    label: 'Lottery',
-    icon: 'TicketIcon',
-    href: '/lottery',
-  },
-  {
-    label: 'Collectibles',
-    icon: 'NftIcon',
-    href: '/collectibles',
-  },
-  {
-    label: 'Team Battle',
-    icon: 'TeamBattleIcon',
-    href: '/competition',
-    status: {
-      text: 'CLAIM',
-      color: 'warning',
-    },
-  },
-  {
-    label: 'Teams & Profile',
-    icon: 'GroupsIcon',
-    items: [
-      {
-        label: 'Leaderboard',
-        href: '/teams',
-      },
-      {
-        label: 'Task Center',
-        href: '/profile/tasks',
-      },
-      {
-        label: 'Your Profile',
-        href: '/profile',
-      },
-    ],
-  },
-  {
     label: 'Info',
     icon: 'InfoIcon',
     items: [
       {
         label: 'Overview',
         href: 'https://pancakeswap.info',
-      },
-      {
-        label: 'Tokens',
-        href: 'https://pancakeswap.info/tokens',
-      },
-      {
-        label: 'Pairs',
-        href: 'https://pancakeswap.info/pairs',
-      },
-      {
-        label: 'Accounts',
-        href: 'https://pancakeswap.info/accounts',
       },
     ],
   },
